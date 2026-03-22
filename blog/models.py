dd471a06c12d5fef2614bd76a11864245b06246b
lafeya-lamos <van.lofei@phystech.edu>
2026-03-22 23:03:36 +0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Article(models.Model):
-    title = models.CharField(max_length=200)      # 标题
-    content = models.TextField()                   # 内容
+    title = models.CharField(max_length=200, verbose_name='单词')      # 添加 verbose_name
+    content = models.TextField(verbose_name='释义')                    # 添加 verbose_name
     pub_date = models.DateTimeField(auto_now_add=True)  # 发布时间
 
     def __str__(self):
