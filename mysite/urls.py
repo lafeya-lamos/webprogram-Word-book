451bea87fb2543a9d8c725944c51ff92ff1cdc0a
lafeya-lamos <van.lofei@phystech.edu>
2026-03-22 22:52:19 +0300

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', views.article_list, name='article_list'),
     path('articles/add/', views.add_article, name='add_article'),
+    path('articles/delete/<int:article_id>/', views.delete_article, name='delete_article'),
 ]
