bd47655acf481a46fd96adbacdd40d2d31ad2f3d
lafeya-lamos <van.lofei@phystech.edu>
2026-03-24 22:24:41 +0300

    style(forms): изменить метки полей

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Article(models.Model):
-    title = models.CharField(max_length=200, verbose_name='单词') 
-    content = models.TextField(verbose_name='释义')                    
+    title = models.CharField(max_length=200, verbose_name='Слово') 
+    content = models.TextField(verbose_name='Значение')                    
     pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
