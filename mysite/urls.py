bd463d2163e11202054e9924fc4aa9e53adac162
lafeya-lamos <van.lofei@phystech.edu>
2026-03-26 19:54:16 +0300

    add(quiz)
    Прописан маршрут /quiz/ в urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('articles/', views.article_list, name='article_list'),
     path('articles/add/', views.add_article, name='add_article'),
     path('articles/delete/<int:article_id>/', views.delete_article, name='delete_article'),
+    path('quiz/', views.quiz_view, name='quiz'),
 ]
