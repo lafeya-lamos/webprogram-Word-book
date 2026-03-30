11d478c29bcc3caa4a1b236f712de1a58c7caed3
lafeya-lamos <van.lofei@phystech.edu>
2026-03-30 21:47:12 +0300

    add(admin): зарегистрировать модели в админ-панели

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
+"""管理面板模块"""
+
 from django.contrib import admin
+
+from .models import Article, WrongWordRecord
+
+admin.site.register(Article)
+admin.site.register(WrongWordRecord)
 
 # Register your models here.
