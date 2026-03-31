0211f8e508f9e0c083d505efd3f748032a840b4a
lafeya-lamos <van.lofei@phystech.edu>
2026-03-31 19:57:41 +0300

    test(models): добавить простые тесты для Article

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,18 @@
+"""测试模块"""
+
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Article
+
+
+class ArticleModelTest(TestCase):
+    """Article模型简单测试"""
+
+    def test_article_creation(self):
+        """创建测试"""
+        article = Article.objects.create(
+            title='Тестовое слово',
+            content='Тестовое значение'
+        )
+        self.assertEqual(article.title, 'Тестовое слово')
+        self.assertEqual(str(article), 'Тестовое слово')
