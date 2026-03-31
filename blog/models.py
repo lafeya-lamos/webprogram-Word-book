8f1b100a47abac1e1aadcdd7f582b274620a6d3d
lafeya-lamos <van.lofei@phystech.edu>
2026-03-31 19:46:37 +0300

    docs(blog): добавить комментарии

--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,21 @@
+"""存储单词和错误记录的模型"""
+
 from django.db import models
 
 class Article(models.Model):
-    title = models.CharField(max_length=200, verbose_name='Слово') 
-    content = models.TextField(verbose_name='Значение')                    
-    pub_date = models.DateTimeField(auto_now_add=True)
+    """单词及释义模型"""
 
-    def __str__(self):
-        return self.title
+    title = models.CharField(max_length=200, verbose_name='Слово')
+    content = models.TextField(verbose_name='Значение')
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
+
+    def __str__(self) -> str:
+        return str(self.title)
 
 class WrongWordRecord(models.Model):
+    """错误记录模型"""
     word = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='wrong_records')
-    wrong_time = models.DateTimeField(auto_now_add=True, verbose_name='答错时间')
+    wrong_time = models.DateTimeField(auto_now_add=True, verbose_name='wrong time')
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.word.title} - {self.wrong_time}"
-
-# Create your models here.
